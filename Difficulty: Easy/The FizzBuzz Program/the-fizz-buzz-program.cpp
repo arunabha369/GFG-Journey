83156123f8da55963af85e102484963c8df608df
Arunabha Banerjee <119969705+arunabha369@users.noreply.github.com>
2025-11-26 15:21:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.cpp
@@ -0,0 +1,15 @@
+void fizzBuzz(int number) {
+
+    if (number % 3 == 0 && number % 5 == 0) {
+        cout << "FizzBuzz\n";
+    }
+    else if (number % 3 == 0) {
+        cout << "Fizz\n";
+    }
+    else if (number % 5 == 0) {
+        cout << "Buzz\n";
+    }
+    else {
+        cout << number << "\n";
+    }
+}
