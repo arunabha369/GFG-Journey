73f135723c67458d88ccffe1e8a0602bdb62e1f1
Arunabha Banerjee <119969705+arunabha369@users.noreply.github.com>
2025-12-19 23:50:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Last Digit of a number/last-digit-of-a-number.cpp
+++ Difficulty: Basic/Last Digit of a number/last-digit-of-a-number.cpp
@@ -1,8 +1,6 @@
-void utility(int n) {
-
-    // just complete below statement
-    int ans = abs(n) % 10;
-
-    // print the result
-    cout << ans << endl;
-}
+class Solution {
+  public:
+    int lastDigit(int n) {
+        return abs(n) % 10;
+    }
+};
