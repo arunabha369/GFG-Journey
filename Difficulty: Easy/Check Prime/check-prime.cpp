7b2c1efba26cfefc50b02fcf46c8805994a14e40
Arunabha Banerjee <119969705+arunabha369@users.noreply.github.com>
2026-01-13 22:59:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check Prime/check-prime.cpp
+++ Difficulty: Easy/Check Prime/check-prime.cpp
@@ -1,11 +1,22 @@
-bool prime(int n) {
+#include <iostream>
+using namespace std;
 
-    if (n <= 1) return false; // 1 and below are not prime
+int main() {
+    int n;
+    cin >> n;
 
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0)
-            return false;     // divisible → not prime
+    if (n <= 1) {
+        cout << "False";
+        return 0;
     }
 
-    return true;              // no divisors → prime
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            cout << "False";
+            return 0;
+        }
+    }
+
+    cout << "True";
+    return 0;
 }
