2158e43cde5414feaf7237edb6d8eb0779d3ea45
Arunabha Banerjee <119969705+arunabha369@users.noreply.github.com>
2026-03-21 23:44:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Bitwise Operators/bitwise-operators.cpp
+++ Difficulty: Basic/Bitwise Operators/bitwise-operators.cpp
@@ -1,17 +1,19 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int a, b, c;
-    cin >> a >> b >> c;
-
+void operators(int a, int b, int c) {
+    // Do a^a below
     int d = a ^ a;
+
+    // Do c^b below
     int e = c ^ b;
+
+    // Do a&b below
     int f = a & b;
+
+    // Do c|(a^a) below
     int g = c | (a ^ a);
+
+    // Do ~e below
     e = ~e;
 
+    // The line below prints the output. Don't change it!
     cout << d << " " << e << " " << f << " " << g << endl;
-
-    return 0;
 }
